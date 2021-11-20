df9df7b58b2da84f6df7fc408695fcb6c4e99441
tobsterclark <tobsterclark@gmail.com>
2021-11-20 18:32:25 +1100

    Updated to using tsparticles instead of particles.js

--- src/components/background.js
+++ src/components/background.js
@@ -1,96 +1,79 @@
 import React, {Component} from 'react'
-import Particles from 'react-particles-js'
+import Particles from 'react-tsparticles'
 
 class Background extends Component{
     render() {
         return(
             <div>
-                <Particles className="bg-black"
+                <Particles
                 params={{
-                    particles: {
-                      number: {
-                        value: 80,
-                        density: {
-                          enable: true,
-                          value_area: 800
-                        }
-                      },
-                      color: {
-                        value: "#4E4E50"
-                      },
-                      shape: {
-                        type: "circle",
-                        stroke: {
-                          width: 0,
-                          color: "#000000"
-                        },
-                        polygon: {
-                          nb_sides: 5
-                        },
-                        image: {
-                          src: "img/github.svg",
-                          width: 100,
-                          height: 100
-                        }
-                      },
-                      opacity: {
-                        value: 0.5,
-                        random: false,
-                        anim: {
-                          enable: false,
-                          speed: 1,
-                          opacity_min: 0.1,
-                          sync: false
-                        }
-                      },
-                      size: {
-                        value: 3,
-                        random: true,
-                        anim: {
-                          enable: false,
-                          speed: 40,
-                          size_min: 0.1,
-                          sync: false
-                        }
-                      },
-                      line_linked: {
-                        enable: true,
-                        distance: 150,
-                        color: "#5c5656",
-                        opacity: 0.4,
-                        width: 1
-                      },
-                      move: {
-                        enable: true,
-                        speed: 1.5,
-                        direction: "none",
-                        random: false,
-                        straight: false,
-                        out_mode: "out",
-                        bounce: false,
-                        attract: {
-                          enable: false,
-                          rotateX: 600,
-                          rotateY: 1200
-                        }
+                  "fullScreen": {
+                    "enable": false,
+                    "zIndex": 0
+                  },
+                  "particles": {
+                    "number": {
+                      "value": 80,
+                      "density": {
+                        "enable": true,
+                        "value_area": 800
                       }
                     },
-                    interactivity: {
-                      detect_on: "canvas",
-                      events: {
-                        onhover: {
-                          enable: false,
-                          mode: "grab"
-                        },
-                        onclick: {
-                          enable: false,
-                          mode: "repulse"
-                        },
-                        resize: true
-                      },
+                    "color": {
+                      "value": "#FFFFFF"
                     },
-                    retina_detect: true
-                  }}/>
+                    "shape": {
+                      "type": "circle"
+                    },
+                    "opacity": {
+                      "value": 1,
+                      "random": false,
+                      "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                      }
+                    },
+                    "size": {
+                      "value": 5,
+                      "random": true,
+                      "anim": {
+                        "enable": true,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                      }
+                    },
+                    "line_linked": {
+                      "enable": true,
+                      "distance": 150,
+                      "color": "#FFFFFF",
+                      "opacity": 1,
+                      "width": 1
+                    },
+                    "move": {
+                      "enable": true,
+                      "speed": 0.7,
+                      "direction": "none",
+                      "random": false,
+                      "straight": false,
+                      "out_mode": "out",
+                      "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                      }
+                    }
+                  },
+                  "retina_detect": true,
+                  "background": {
+                    "color": "#1A1A1D",
+                    "position": "50% 50%",
+                    "repeat": "no-repeat",
+                    "size": "cover"
+                  }
+                }}/>
             </div>
         )
     }
